694176f9173cd2729f4189d1ff9727e2243e459a
kengo-saito <k.saitoh0526@gmail.com>
2023-03-23 10:04:50 +0900

    Initial Commit

---
+++ WhatsDish/WhatsDishApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsDishApp.swift
+//  WhatsDish
+//
+//  Created by 齋藤健悟 on 2023/03/23.
+//
+
+import SwiftUI
+
+@main
+struct WhatsDishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
